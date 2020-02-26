a158c4ceb82d0434038fe807b8c8af09d7d74ea9
Daniel H Cohen <hofcohen@gmail.com>
2020-02-26 22:09:00 +0000

    updating problems solved in euler project and my about me

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const request = new XMLHttpRequest()
+const request2 = new XMLHttpRequest()
 const codewarsHonor = document.getElementById("codewars")
+const projectEuler = document.getElementById("projectEuler")
 
-const url='https://cors-anywhere.herokuapp.com/https://www.codewars.com/api/v1/users/Mathsteacher7?access_key=Su-FKGsHrjGLgYsinWLA';
 
-request.open('GET', url, true)
-
-request.onload = function() {
-    const data = JSON.parse(this.response)
-    const honor = data.honor
-    codewarsHonor.textContent += honor
-
+function honor() {
+    const codeWarsURL='https://cors-anywhere.herokuapp.com/https://www.codewars.com/api/v1/users/Mathsteacher7?access_key=Su-FKGsHrjGLgYsinWLA';
+    request.open('GET', codeWarsURL, true)
+    request.onload = function() {
+        const data = JSON.parse(this.response)
+        const honor = data.honor
+        codewarsHonor.textContent += honor
+    
+    }
 }
+honor()
 request.send()
 
+function euler() {
+    const projectEulerURL='https://cors-anywhere.herokuapp.com/http://projecteuler.net/profile/mathsteacher7.txt';
+    request2.open('GET', projectEulerURL, true)
+    
+    request2.onload = function() {
+        const data = JSON.stringify(this.response)
+        const answered = data.slice(24, 26)
+        projectEuler.textContent += answered
+        
+    }
+}
+euler()
+request2.send()
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-
-// }
-// )
 
